3e4f240c2c25fad4ca4ba3bdd0282a76aa65c1ba
Kartoshka548 <kartoshka548@gmail.com>
2013-01-31 21:19:41 +0200

    Data is shown. Added styling+base, static content (css) is served via STATIC_URL (RequestContext and django.core.context_processors.static CONTEXT PROCESSOR

--- ARPASOPROFILE/settings.py
+++ ARPASOPROFILE/settings.py
@@ -1,3 +1,4 @@
+from django.conf import global_settings
 # Django settings for ARPASOPROFILE project.
 
 # kartomodification
@@ -60,7 +61,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '' 
+# OTHERWISE: os.path.join(DIR, 'static') >> ImproperlyConfigured: The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,6 +73,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(DIR, 'static'),
+    # '/users/Kartoshka/Sites/Python/ARPASOPROFILE/PROFILE/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -110,6 +114,10 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.static',
 )
 
 INSTALLED_APPS = (
--- PROFILE/views.py
+++ PROFILE/views.py
@@ -1,9 +1,9 @@
-from django.template import loader, Context
+from django.template import loader, RequestContext
 from django.http import HttpResponse
 from PROFILE.models import UserProfile
 
 def display_data(request):
     profiles = UserProfile.objects.all()
     a_template = loader.get_template("userdata.html")
-    a_context = Context({ "arpaso_profiles" : profiles })
+    a_context = RequestContext(request, { "arpaso_profiles" : profiles })
     return HttpResponse(a_template.render(a_context))
