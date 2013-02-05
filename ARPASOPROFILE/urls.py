6f70d7e296f5bd08effd88a950bb43c472e9f213
Kartoshka548 <kartoshka548@gmail.com>
2013-02-05 13:47:51 +0200

    minor changes to redirects and css styling. No real functional refactoring

--- ARPASOPROFILE/urls.py
+++ ARPASOPROFILE/urls.py
@@ -5,10 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login_url"),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/list/'}, name="logout_url"),
+    url(r'^login/$', 'django.contrib.auth.views.login', name="login_url"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="logout_url"),
 
     # forward control into local app
-    url(r'^list/', include('PROFILE.urls')),
     url(r'^$', include('PROFILE.urls')),
     )
--- PROFILE/urls.py
+++ PROFILE/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('PROFILE.views',
-    url(r'^$', 'display_data', name='url_display_data'),
+    url(r'$', 'display_data', name='url_display_data'),
     )
